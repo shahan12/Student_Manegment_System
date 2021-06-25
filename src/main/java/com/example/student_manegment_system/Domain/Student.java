af76d9837750aecc3ea81e12299f949dce85161d
Shahan Khan <shahankhan485@gmail.com>
2021-06-25 11:34:27 +0530

    first commit

---
+++ src/main/java/com/example/student_manegment_system/Domain/Student.java
@@ -0,0 +1,59 @@
+package com.example.student_manegment_system.Domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String studentname;
+    private String course;
+    private int fee;
+
+    public Student() {
+        super();
+    }
+
+    public Student(long id, String studentname, String course, int fee) {
+        this.id = id;
+        this.studentname = studentname;
+        this.course = course;
+        this.fee = fee;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStudentname() {
+        return studentname;
+    }
+
+    public void setStudentname(String studentname) {
+        this.studentname = studentname;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
+    public void setFee(int fee) {
+        this.fee = fee;
+    }
+}
---
+++ src/main/java/com/example/student_manegment_system/Service/StudentService.java
@@ -0,0 +1,34 @@
+package com.example.student_manegment_system.Service;
+
+import com.example.student_manegment_system.DAO.StudentRepository;
+import com.example.student_manegment_system.Domain.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+@Service
+    public class StudentService {
+
+        @Autowired
+        private StudentRepository repo;
+
+        public List<Student> listAll() {
+            return repo.findAll();
+        }
+
+        public void save(Student std) {
+            repo.save(std);
+        }
+
+        public Student get(long id) {
+            return repo.findById(id).get();
+        }
+
+        public void delete(long id) {
+            repo.deleteById(id);
+        }
+
+    }
+
